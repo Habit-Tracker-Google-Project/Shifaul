4e3ad7d5d9e6a765d2773778456df1f6e27543eb
shifaulemdad <113154571+shifaulemdad@users.noreply.github.com>
2023-05-12 07:02:30 -0400

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,1 +1,14 @@
-console.log("This is a popup!")
+//console.log("This is a popup!")
+
+chrome.storage.session.get(null, function(items) {
+    var table = document.getElementById("websiteData");
+    for (key in items) {
+       var row = table.insertRow();
+       var urlCell = row.insertCell(0);
+       var countCell = row.insertCell(1);
+       urlCell.innerHTML = key;
+       countCell.innerHTML = items[key];
+    }
+    // populate the table with data retrieved from session storage
+ });
+ 
