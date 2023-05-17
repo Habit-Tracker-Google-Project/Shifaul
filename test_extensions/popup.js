1fe737e6cbd1afbc4e404351d7e35b6edf52184b
shifaulemdad <113154571+shifaulemdad@users.noreply.github.com>
2023-05-17 00:34:57 -0400

    Add files via upload

--- test_extensions/popup.js
+++ test_extensions/popup.js
@@ -1,3 +1,29 @@
+<<<<<<< HEAD
+console.log("This is a popup!")
+
+
+chrome.storage.session.get(null, function(items) {
+   const div = document.getElementById("splits");
+   const table = document.createElement("table");
+   
+   Object.entries(items).forEach(([key, item]) => {
+      var row = document.createElement("tr");
+
+      var urlCell = document.createElement("td");
+      urlCell.append(key);
+
+      var countCell = document.createElement("td");
+      countCell.append(item);
+
+      row.append(urlCell);
+      row.append(countCell);
+      table.append(row);
+   });
+
+   div.append(table);
+});
+
+=======
 //console.log("This is a popup!")
 
 chrome.storage.session.get(null, function(items) {
@@ -21,3 +47,4 @@ chrome.storage.session.get(null, function(items) {
 
    div.append(table);
 });
+>>>>>>> 934fa0a1d358b45b750ea83c946e71047d1783a3
