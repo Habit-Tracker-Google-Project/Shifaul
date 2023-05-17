943287989515746c5e7536438c1f8b6f19e6b5ba
shifaulemdad <113154571+shifaulemdad@users.noreply.github.com>
2023-05-17 00:39:20 -0400

    Add files via upload

--- test_extensions/popup.js
+++ test_extensions/popup.js
@@ -1,50 +1,26 @@
-<<<<<<< HEAD
 console.log("This is a popup!")
 
-
 chrome.storage.session.get(null, function(items) {
    const div = document.getElementById("splits");
    const table = document.createElement("table");
-   
-   Object.entries(items).forEach(([key, item]) => {
+   for (const key in items) {
+      console.log(key);
+      console.log(items[key]);
       var row = document.createElement("tr");
 
       var urlCell = document.createElement("td");
       urlCell.append(key);
 
       var countCell = document.createElement("td");
+      var item = items[key];
       countCell.append(item);
 
       row.append(urlCell);
       row.append(countCell);
       table.append(row);
-   });
+      div.append(table);
 
-   div.append(table);
+   }
+   // populate the table with data retrieved from session storage
 });
-
-=======
-//console.log("This is a popup!")
-
-chrome.storage.session.get(null, function(items) {
-   const div = document.getElementById("splits");
-   const table = document.createElement("table");
-   
-   // Iterate over the entries of the items object
-   Object.entries(items).forEach(([key, item]) => {
-      var row = document.createElement("tr");
-
-      var urlCell = document.createElement("td");
-      urlCell.append(key);
-
-      var countCell = document.createElement("td");
-      countCell.append(item);
-
-      row.append(urlCell);
-      row.append(countCell);
-      table.append(row);
-   });
-
-   div.append(table);
-});
->>>>>>> 934fa0a1d358b45b750ea83c946e71047d1783a3
+ 
